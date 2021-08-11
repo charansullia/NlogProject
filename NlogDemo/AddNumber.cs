b566d28a86d02576e38bbe3fa85d4d4f89715f36
charasullia <charanrajr713@gmail.com>
2021-08-11 12:06:12 +0530

    [charansullia]added:Nlog project

---
+++ NlogDemo/AddNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NLog;
+
+namespace NlogDemo
+{
+    class AddNumber
+    {
+        static NLog nlog = new NLog();
+        public int Addition(int firstnumber, int secondnumber)
+        {
+
+            if (firstnumber == 0 || secondnumber == 0)
+            {
+
+                nlog.LogInfo("Sub method");
+                nlog.LogDebug("Debug Successful");
+                nlog.LogError("First number or second number is zero");
+                return 0;
+            }
+            int result = firstnumber + secondnumber;
+            nlog.LogInfo("Sub method");
+            nlog.LogDebug("Debug Successful");
+            nlog.LogWarn("Warning");
+            Console.WriteLine(result);
+            return result;
+        }
+    }
+}
+    
---
+++ NlogDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NlogDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddNumber sum = new AddNumber();
+            sum.Addition(10, 12);
+        }
+    }
+}
